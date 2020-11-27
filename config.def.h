5b313c407d1f68ffe3898a875d3ef56461698b5d
Marcus Zanona <marcus@zanona.co>
2020-11-27 19:38:46 +0100

    feat: update color scheme based on edge
    https://github.com/sainnhe/edge

--- config.def.h
+++ config.def.h
@@ -5,8 +5,8 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
-static int borderpx = 2;
+static char *font = "Monospace:size=10:antialias=true:autohint=true";
+static int borderpx = 50;
 
 /*
  * What program is execed by st depends of these precedence rules:
@@ -96,30 +96,27 @@ unsigned int tabspaces = 8;
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
 	/* 8 normal colors */
-	"black",
-	"red3",
-	"green3",
-	"yellow3",
-	"blue2",
-	"magenta3",
-	"cyan3",
-	"gray90",
-
-	/* 8 bright colors */
-	"gray50",
-	"red",
-	"green",
-	"yellow",
-	"#5c5cff",
-	"magenta",
-	"cyan",
-	"white",
-
+	"#88909f",
+	"#f07e7e",
+	"#a3bc8d",
+	"#d1b277",
+	"#7ab3e1",
+	"#d898eb",
+	"#78b7b1",
+	"#bcc4d1",
+	"#88909f",
+	"#f07e7e",
+	"#a3bc8d",
+	"#d1b277",
+	"#7ab3e1",
+	"#d898eb",
+	"#78b7b1",
+	"#bcc4d1",
 	[255] = 0,
-
-	/* more colors can be added after 255 to use with DefaultXX */
-	"#cccccc",
-	"#555555",
+	"#bcc4d1",
+	"#000000",
+	"#bcc4d1",
+	"#88909f",
 };
 
 
@@ -127,10 +124,10 @@ static const char *colorname[] = {
  * Default colors (colorname index)
  * foreground, background, cursor, reverse cursor
  */
-unsigned int defaultfg = 7;
-unsigned int defaultbg = 0;
-static unsigned int defaultcs = 256;
-static unsigned int defaultrcs = 257;
+unsigned int defaultfg = 256;
+unsigned int defaultbg = 257;
+static unsigned int defaultcs = 258;
+static unsigned int defaultrcs = 259;
 
 /*
  * Default shape of cursor
